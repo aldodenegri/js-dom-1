183bb8d927dd1c91e2f47b9ae2ede8710ff041c3
aldodenegri <aldodenegri00@gmail.com>
2025-11-10 20:03:02 +0100

    inserimento di metodo aggiuntivo per lo switch acceso/spento

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 function switchLamp(lampId) {
-            const lamp = document.getElementById(lampId);
+    const lamp = document.getElementById(lampId);
 
-            const currentImg = lamp.querySelector('img').src;
+    const currentImg = lamp.querySelector('img').src;
 
-            if (currentImg.includes('white_lamp.png')) {
-                
-                lamp.innerHTML = `
+    if (currentImg.includes('white_lamp.png')) {
+
+        lamp.innerHTML = `
                     <img src="./img/yellow_lamp.png" alt="onLamp" class="img-fluid">
                     <button class="btn btn-secondary mt-2" onclick="switchLamp('${lampId}')">
                         Turn Off
                     </button>
                 `;
-            } else {
-                
-                lamp.innerHTML = `
+    } else {
+
+        lamp.innerHTML = `
                     <img src="./img/white_lamp.png" alt="offLamp" class="img-fluid">
                     <button class="btn btn-warning mt-2" onclick="switchLamp('${lampId}')">
                         Turn On
                     </button>
                 `;
-            }
-        }
+    }
+}
+function switchLamp_alt(lampId, turnOn) {
+    const container = document.getElementById(lampId);
+    const divs = container.children;
+
+    if (turnOn) {
+
+        divs[0].classList.add('d-none');
+        divs[1].classList.remove('d-none');
+    } else {
+
+        divs[0].classList.remove('d-none');
+        divs[1].classList.add('d-none');
+    }
+}
+
