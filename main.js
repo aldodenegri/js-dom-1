da067f896216d379cef4e46c5f27fa27f0656e40
aldodenegri <aldodenegri00@gmail.com>
2025-11-10 19:37:41 +0100

    cambio funzione main.js, gli id non cambiavano e il funzionamento era scorretto

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-function switchFunction1() {
-    document.getElementById("lamp1").innerHTML = `
-        <img src="./img/yellow_lamp.png" alt="onLamp" class="img-fluid">
-        <button class="btn btn-secondary mt-2" onclick="switchFunction2()">Turn Off</button>
-      `;
-    document.getElemenntById("lamp1").id= "lamp2";
-}
+function switchLamp(lampId) {
+            const lamp = document.getElementById(lampId);
 
-function switchFunction2() {
-    document.getElementById("lamp2").innerHTML = `
-        <img src="./img/white_lamp.png" alt="offLamp" class="img-fluid">
-        <button class="btn btn-warning mt-2" onclick="switchFunction1()">Turn On</button>
-      `;
-      document.getElemenntById("lamp2").id= "lamp1";
-}
+            const currentImg = lamp.querySelector('img').src;
+
+            if (currentImg.includes('white_lamp.png')) {
+                
+                lamp.innerHTML = `
+                    <img src="./img/yellow_lamp.png" alt="onLamp" class="img-fluid">
+                    <button class="btn btn-secondary mt-2" onclick="switchLamp('${lampId}')">
+                        Turn Off
+                    </button>
+                `;
+            } else {
+                
+                lamp.innerHTML = `
+                    <img src="./img/white_lamp.png" alt="offLamp" class="img-fluid">
+                    <button class="btn btn-warning mt-2" onclick="switchLamp('${lampId}')">
+                        Turn On
+                    </button>
+                `;
+            }
+        }
